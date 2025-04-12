3b8f22db49665d11ceb15568a0ee923527370731
aaravhajare <hajareaarav66@gmail.com>
2025-04-12 11:59:15 +0530

    Get the country code

---
+++ Get the country code.py
@@ -0,0 +1,9 @@
+country_code = {
+    "india" : "IN",
+    "neapal" : "np",
+    "germany" : "DE",
+}
+
+print("country code for india is" , country_code.get("india" , "not found"))
+
+print("country code for japan is" , country_code.get("japan" , "not found"))
